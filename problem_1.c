95d6084b3d4df1e1275808aff2e652c29561725e
BruceFrankWang <bruce.frank.wang@me.com>
2016-10-24 23:38:04 +0800

    Problem 1
    Correct.

---
+++ problem_1.c
@@ -0,0 +1,32 @@
+// Problem 1 @ <Project Euler>
+
+#include <stdio.h>
+#include <time.h>
+
+int main(void)
+{
+    clock_t start, finish;
+    double  duration;
+    int result = 0;
+    
+    start = clock();
+    
+    for (int i=1; i<1000; i++) {
+        if (i % 3 == 0) {
+            result += i;
+        }
+        if (i % 5 == 0) {
+            result += i;
+        }
+        if (i % 15 == 0) {
+            result -= i;
+        }
+    }
+    printf("%d\n", result);
+    
+    finish = clock();
+    duration = (double)(finish - start) / CLOCKS_PER_SEC;
+    printf( "%f seconds\n", duration );
+    
+    return 0;
+}
