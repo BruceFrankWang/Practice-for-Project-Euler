ca6fa773720665bb0ccadae430801d893172b6d3
BruceFrankWang <bruce.frank.wang@me.com>
2016-11-07 15:45:28 +0800

    Change to using "pelib".

--- problem_0036.c
+++ problem_0036.c
@@ -4,13 +4,10 @@
 #include <time.h>
 #include <string.h>
 #include <stdlib.h>
-#include "bignumber.h"
+
+#include "pelib.h"
 
 #define MAX 1000000
-
-int isPalindrome(const char *src);
-char *trim_double(const char *src, size_t from, size_t to);
-char *trim_single(const char *src, int remove);
 
 int main(void) {
     clock_t start, finish;
@@ -35,64 +32,11 @@ int main(void) {
         sum += n;
         printf("\rDouble-base palindrome: %ld\n", n);
     }
-    printf("The sum is: %lld\n", sum);
+    printf("\rThe sum is: %lld\n", sum);
     
     finish = clock();
     duration = (double)(finish - start) / CLOCKS_PER_SEC;
     printf("%f seconds\n", duration);
     
     return 0;
-}
-
-int isPalindrome(const char *src) {
-    size_t len = strlen(src);
-    if (len == 0) {
-        return 0;
-    }
-    char *temp = (char *)malloc(sizeof(char) * (len + 1));
-    for (size_t i = 0, j = len-1; i < len; i++, j--) {
-        temp[i] = src[j];
-    }
-    temp[len] = '\0';
-    if (strcmp(src, temp) == 0) {
-        return 1;
-    } else {
-        return 0;
-    }
-}
-
-char *trim_double(const char *src, size_t trim_left, size_t trim_right) {
-    size_t len = strlen(src);
-    if (trim_left >= len || trim_left > trim_right) {
-        return NULL;
-    }
-    if (trim_right >= len) {
-        trim_right = len - 1;
-    }
-    size_t l = trim_right - trim_left + 1;
-    size_t i, j;
-    char *dest = (char *)malloc(sizeof(char) * (l + 1));
-    int z = sizeof(dest)/sizeof(char);
-    for(i=trim_left, j=0; i<=trim_right; i++, j++) {
-        dest[j] = src[i];
-    }
-    dest[l] = '\0';
-    return dest;
-}
-
-char *trim_single(const char *src, int remove) {
-    size_t len = strlen(src);
-    if ((remove > 0 && remove >= len) || (remove < 0 && remove <= -len)) {
-        return NULL;
-    }
-    if (remove == 0) {
-        return trim_double(src, 0, len-1);
-    }
-    if (remove > 0) {
-        return trim_double(src, remove, len-1);
-    }
-    if (remove < 0) {
-        return trim_double(src, 0, len-1 + remove);
-    }
-    return NULL;
 }
--- problem_0036.makefile
+++ problem_0036.makefile
@@ -1,16 +1,16 @@
-pe0036 : problem_0036.o bignumber.o
-	cc -std=c11 -o problem_0036 problem_0036.o bignumber.o
+pe0036 : problem_0036.o pelib.o
+	cc -std=c11 -o problem_0036 problem_0036.o pelib.o
 
-problem_0036.o : problem_0036.c bignumber.h bignumber.c
+problem_0036.o : problem_0036.c pelib.h pelib.c
 	cc -std=c11 -c problem_0036.c
 
-bignumber.o : bignumber.h bignumber.c
-	cc -std=c11 -c bignumber.c
+pelib.o : pelib.h pelib.c
+	cc -std=c11 -c pelib.c
 
 .PHONY : clean
 clean :
-	-rm problem_0036.o bignumber.o
+	-rm problem_0036.o pelib.o
 
 .PHONY : cleanall
 cleanall :
-	-rm problem_0036 problem_0036.o bignumber.o
+	-rm problem_0036 problem_0036.o pelib.o
