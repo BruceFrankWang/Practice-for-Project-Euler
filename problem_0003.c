035a78799c10290cdf35408ee14334a695b4432f
BruceFrankWang <bruce.frank.wang@me.com>
2016-11-07 16:20:20 +0800

    Change to using "pelib".

--- problem_0003.c
+++ problem_0003.c
@@ -4,27 +4,20 @@
 #include <time.h>
 #include <math.h>
 
+#include "pelib.h"
+
 #define NUMBER 600851475143
 
-int is_prime(long n)
-{
-    for (long i=2; i<((long)sqrt(n)+1); i++) {
-        if (n % i == 0) {
-            return 0;
-        }
-    }
-    return 1;
-}
 int main(void)
 {
     clock_t start, finish;
     double  duration;
-    long i=1, result=0, x;
-    
     start = clock();
     
+    long i=1, result=0, x;
+    
     for (long i=2; i < ((long)sqrt(NUMBER)+1); i++) {
-        if (is_prime(i) == 1 && NUMBER % i == 0) {
+        if (isPrime(i) == 1 && NUMBER % i == 0) {
             x = NUMBER;
             while (x != i) {
                 if (x % i == 0) {
